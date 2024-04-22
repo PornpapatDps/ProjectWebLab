7c0ca16c90b81b02ab8ca8884b9161cff2d0e7c3
Pornpapat <pornpapat.d@ku.th>
2024-04-23 02:41:50 +0700

    Update TableData.js

--- fontend/TableData.js
+++ fontend/TableData.js
@@ -10,6 +10,26 @@ const loadData = async () => {
     const response = await axios.get(`${BASE_URL}/users`)
     console.log(response.data);
 
+    let menu = document.querySelector('#menu-icon');
+    let navlist = document.querySelector('.navlist');
+
+    menu.onclick = ()=>{
+        menu.classList.toggle('bx-x');
+        navlist.classList.toggle('open');
+    };
+
+    const sr = ScrollReveal({
+        distance:'65px',
+        duration:2600,
+        delay:450,
+        reset:true
+    });
+    sr.reveal('.hero-text',{deplay:200,origin:'top'});
+    sr.reveal('.hero-img',{deplay:450,origin:'top'});
+    sr.reveal('.icons',{deplay:500,origin:'left'});
+    sr.reveal('.scroll-down',{deplay:500,origin:'right'});
+
+ 
     const userDOM = document.getElementById('user')
 
     let htmlData = '<div>'
